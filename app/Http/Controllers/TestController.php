4e2d2490151ebb2aff4e03d7fb3b0c3e687ae3e1
Aditya Kadam <adityakadam@Adityas-MacBook-Air.local>
2023-11-13 20:35:56 +0000

    laravel react session

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -2,12 +2,21 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use Illuminate\Http\Request;
+use Inertia\Inertia;
 
 class TestController extends Controller
 {
-    public function index(){
-        return "This is about";
+    public function index(Request $request){
+        // dd($request->search);
+        // pattern match using sql like operator 
+        $users = User::where('name', 'like', "%".$request->search."%")->paginate(10);
+        // dd($users);
+        // select * from users
+        // select * from users skip 40 limit 10
+        return Inertia::render("About", ['users' => $users]);
+
     }
 
     public function subscribe(Request $request)
